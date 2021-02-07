2618a990d6e996e5dc9d89cd6661952d59cbf5bd
devb0x <moniezgeoffrey59@gmail.com>
2021-02-07 09:48:52 +0100

    header end

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const hamburger_btn = document.getElementById('btnHamburger')
+const hero = document.querySelector('.hero');
+const title_h1 = document.querySelector('.main-title');
+const headerLinks_div = document.querySelector('.header__links');
+
+hamburger_btn.addEventListener('click', () => {
+  console.log('clic!');
+  if (hero.classList.contains('open')) {
+    hero.classList.remove('open');
+    title_h1.style.display = 'block';
+    headerLinks_div.classList.remove('linkTest');
+
+
+  } else {
+    hero.classList.add('open');
+    title_h1.style.display = 'none';
+    headerLinks_div.classList.add('linkTest');
+  }
+});
